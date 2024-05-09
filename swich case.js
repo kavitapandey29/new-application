3479788c6ab3fb5d4b816a45e254de5c3b09b1e8
kavitapandey29 <157573592+kavitapandey29@users.noreply.github.com>
2024-05-09 19:52:18 +0530

    Add files via upload

---
+++ swich case.js
@@ -0,0 +1,36 @@
+ // date 30-Jan-2024
+  
+ //swich case- expression matched
+
+//Q. print name with the help of swich case
+let name ="kavita"
+
+switch(name){
+   case"soniya":
+   console.log("hello soniya");
+   break;
+   case"megha":
+   console.log("hello megha");
+   break;
+    case"kavita":
+   console.log("hello kavita");
+   break;
+   case"hema":
+   console.log("hello hema");
+   break;
+   default:
+       console.log("invalid or error");
+}
+
+
+//Q. print both the console with the help of swich case
+let age ="24"
+switch(age){
+   case"24":
+   console.log("you are 24");
+   case "24":
+   console.log("you are number 24");
+   break;
+   default:
+   console.log("error");
+}
